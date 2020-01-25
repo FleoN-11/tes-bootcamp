15e54bd8945fe91187453b05cf51278282d9f865
FleoN-11 <54788803+FleoN-11@users.noreply.github.com>
2020-01-25 22:03:57 +0700

    Add files via upload

---
+++ cek.py
@@ -0,0 +1,11 @@
+string input = "assfddssgddhsdg";
+    map<character, integer> map = new HashMap<>();
+
+    for (int i=0; i < input.length(); ++i){
+        integer count = map.get(input.charAt(i));
+        map.put(input.charAt(i), count == null ? 1 : count + 1);
+    }
+
+    map.entrySet().stream()
+        .filter(entry -> entry.getValue() > 1)
+        .forEach(System.out::println);
